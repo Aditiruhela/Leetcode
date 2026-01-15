f4f74ffac8bd5ddc536796c264de2462262c613f
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-01-15 11:15:02 +0530

    Time: 0 ms (100%), Space: 44.4 MB (21.86%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -8,18 +8,23 @@ class Solution {
         // }
         // if(arr[n-1]>arr[n-2]) return n-1;
         // return -1;
-        int start=1;
-        int end=n-2;
-        if(n==1) return 0;
-        if(arr[0]>arr[1]) return 0;
-        if(arr[n-1]>arr[n-2]) return n-1;
-        while(start<=end){
-        int mid=start+(end-start)/2;
-        if(arr[mid]>arr[mid-1] && arr[mid+1]<arr[mid]) return mid;
-        else if(arr[mid]>arr[mid-1]) start=mid+1;
-        else if(arr[mid]>arr[mid+1]) end=mid-1;
-        else start=mid+1;
+
+        // int start=1;
+        // int end=n-2;
+        // if(n==1) return 0;
+        // if(arr[0]>arr[1]) return 0;
+        // if(arr[n-1]>arr[n-2]) return n-1;
+        // while(start<=end){
+        // int mid=start+(end-start)/2;
+        // if(arr[mid]>arr[mid-1] && arr[mid+1]<arr[mid]) return mid;
+        // else if(arr[mid]>arr[mid-1]) start=mid+1;
+        // else if(arr[mid]>arr[mid+1]) end=mid-1;
+        // else start=mid+1;
+        // }
+        // return -1;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]) return i;
         }
-        return -1;
+        return n-1;
     }
 }
