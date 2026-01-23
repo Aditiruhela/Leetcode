50e7c184bba580340a278478c7f989b2d0608320
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-01-23 10:52:03 +0530

    Time: 14 ms (32.67%), Space: 46.5 MB (26.28%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase().replaceAll("[^a-z0-9]",""); //^-->not(atoz alphabets,0 to 9 digits) ko replace krdo ""black se  called regex criteria
-        int i=0;
-        int j=s.length()-1;
+    s=s.toLowerCase().replaceAll("[^a-z0-9]","");
+        int n=s.length();
+        int i=0,j=n-1;
         while(i<j){
             if(s.charAt(i)!=s.charAt(j)) return false;
             i++;
