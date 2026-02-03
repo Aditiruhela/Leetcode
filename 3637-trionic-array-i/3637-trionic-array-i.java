7a79ecc6225b7808ed3a7206c824eb08f0d95e7f
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-02-03 20:22:04 +0530

    Time: 3 ms (7.98%), Space: 44.7 MB (64.71%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -0,0 +1,59 @@
+// class Solution {
+//     public boolean isIncreasing(int[]arr,int i,int j){
+//         for(int k=i;k<j;k++){
+//         if(arr[k]<=arr[k+1])return true;
+//         }
+//         return false;
+//     }
+//     public boolean isDecreasing(int[]arr,int i,int j){
+//         for(int k=i;k<j;k++){
+//         if(arr[k]>=arr[k+1]) return true;
+//         }
+//         return false;
+//     }
+//     public boolean isTrionic(int[] arr) {
+//         int n=arr.length;
+//         if(n<4) return false;
+//         for(int i=1;i<=n-3;i++){
+//             for(int j=i+1;j<=n-2;j++){
+//                 if(!isIncreasing(arr,0,i)) continue;
+//                 if(!isDecreasing(arr,i,j)) continue;
+//                 if(!isIncreasing(arr,j,n-1)) continue;
+//                 return true;
+
+//             }
+//         }
+//         return false;
+//     }
+// }
+class Solution {
+
+    public boolean isIncreasing(int[] arr, int i, int j) {
+        for (int k = i; k < j; k++) {
+            if (arr[k] >= arr[k + 1]) return false;
+        }
+        return true;
+    }
+
+    public boolean isDecreasing(int[] arr, int i, int j) {
+        for (int k = i; k < j; k++) {
+            if (arr[k] <= arr[k + 1]) return false;
+        }
+        return true;
+    }
+
+    public boolean isTrionic(int[] arr) {
+        int n = arr.length;
+        if (n < 4) return false;
+
+        for (int i = 1; i <= n - 3; i++) {
+            for (int j = i + 1; j <= n - 2; j++) {
+                if (!isIncreasing(arr, 0, i)) continue;
+                if (!isDecreasing(arr, i, j)) continue;
+                if (!isIncreasing(arr, j, n - 1)) continue;
+                return true;
+            }
+        }
+        return false;
+    }
+}
