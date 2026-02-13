67f705bcdc26e4d06e57cda1ca09e313ed83edca
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-02-13 11:42:43 +0530

    Time: 2 ms (95.13%), Space: 45.8 MB (50.96%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,34 @@
+import java.util.*;
+
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), candidates, target, 0);
-        return result;
+
+    public List<List<Integer>> combinationSum(int[] arr, int x) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, arr, x, 0, res, new ArrayList<>());
+        return res;
     }
 
-    public void backtrack(List<List<Integer>> result, List<Integer> temp, 
-                          int[] candidates, int target, int start) {
-        
-        if (target == 0) {
-            result.add(new ArrayList<>(temp));
+    public void backtrack(int ind, int[] arr, int x, int sum,
+                          List<List<Integer>> res,
+                          List<Integer> curr) {
+
+        // Base case: if sum becomes equal to target
+        if (sum == x) {
+            res.add(new ArrayList<>(curr));
             return;
         }
 
-        if (target < 0) return;
-
-        for (int i = start; i < candidates.length; i++) {
-            temp.add(candidates[i]);
-            backtrack(result, temp, candidates, target - candidates[i], i); 
-            temp.remove(temp.size() - 1); // backtrack
+        // If sum exceeds target OR index out of bounds
+        if (sum > x || ind == arr.length) {
+            return;
         }
+
+        // -------- Take the element --------
+        curr.add(arr[ind]);
+        backtrack(ind, arr, x, sum + arr[ind], res, curr);  
+        curr.remove(curr.size() - 1);   // backtrack
+
+        // -------- Skip the element --------
+        backtrack(ind + 1, arr, x, sum, res, curr);
     }
 }
