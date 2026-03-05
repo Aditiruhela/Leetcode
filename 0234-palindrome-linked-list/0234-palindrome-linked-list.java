fa08c58d437cec30b243e0c451f7a8031d0d4c06
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-03-05 17:41:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,31 +10,31 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        // ArrayList<Integer>list=new ArrayList<>();
-        // ListNode temp=head;
-        // while(temp!=null){
-        //     list.add(temp.val);
-        //     temp=temp.next;
-        // }
-        // int i=0;
-        // int j=list.size()-1;
-        // while(i<j){
-        //     if(list.get(i)!=list.get(j)) return false;
-        //         i++;
-        //         j--;
-        // }
-        // return true;
-        Stack <Integer>st=new Stack<>();
+        ArrayList<Integer>list=new ArrayList<>();
         ListNode temp=head;
         while(temp!=null){
-            st.push(temp.val);
+            list.add(temp.val);
             temp=temp.next;
         }
-        temp=head;
-        while(temp!=null){
-            if(st.pop()!=temp.val) return false;
-            else temp=temp.next;
+        int i=0;
+        int j=list.size()-1;
+        while(i<j){
+            if(list.get(i)!=list.get(j)) return false;
+                i++;
+                j--;
         }
         return true;
+    //     Stack <Integer>st=new Stack<>();
+    //     ListNode temp=head;
+    //     while(temp!=null){
+    //         st.push(temp.val);
+    //         temp=temp.next;
+    //     }
+    //     temp=head;
+    //     while(temp!=null){
+    //         if(st.pop()!=temp.val) return false;
+    //         else temp=temp.next;
+    //     }
+    //     return true;
     }
 }
