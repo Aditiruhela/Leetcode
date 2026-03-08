e4987566e00c3d84ebb91b331a61c05913db8ded
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-03-09 00:04:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,12 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-    s=s.toLowerCase().replaceAll("[^a-z0-9]","");
-        int n=s.length();
+        s=s.toLowerCase().replaceAll("[^a-z0-9]","");
+        String[] words=s.split("");
+        StringBuilder sb=new StringBuilder();
+        int n=words.length;
+        for(int i=0;i<n;i++){
+            sb.append(words[i]);
+        }
         int i=0,j=n-1;
         while(i<j){
-            if(s.charAt(i)!=s.charAt(j)) return false;
-            i++;
-            j--;
+            if(sb.charAt(i)==sb.charAt(j)){
+                i++;
+                j--;
+            }else{
+                return false;
+            }
         }
         return true;
     }
