b0c5d5e1d1fc49610f3fb207e58cb986b44f5038
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-03-09 16:27:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestConsecutive(int[] arr) {
+        int n=arr.length;
+        if(n==0) return 0;
+        TreeSet<Integer>set=new TreeSet<>();
+        for(int x:arr) set.add(x);
+        int count=1;
+        int max=1;
+        ArrayList<Integer>list=new ArrayList<>(set);
+        for(int i=0;i<list.size()-1;i++){
+            if(list.get(i)+1==list.get(i+1)) count++;
+            else break;
+            if(count>max) max=count;
+        }
+        return count;
+    }
+}
