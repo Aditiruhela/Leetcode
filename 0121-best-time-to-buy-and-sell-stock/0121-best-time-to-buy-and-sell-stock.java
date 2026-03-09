4b1201bf53ba7f577c98c74f3c5631957c9a488b
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-03-09 17:50:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,21 +1,29 @@
 class Solution {
     public int maxProfit(int[] arr) {
-        int min=Integer.MAX_VALUE;
+        // int min=Integer.MAX_VALUE;
         int n=arr.length;
-        int[]ans=new int[n];
-        for(int i=0;i<n;i++){
-            min=Math.min(arr[i],min);
-            ans[i]=min;
+        // int[]ans=new int[n];
+        // for(int i=0;i<n;i++){
+        //     min=Math.min(arr[i],min);
+        //     ans[i]=min;
 
-        }
-        int x=0;
+        // }
+        // int x=0;
         int max=Integer.MIN_VALUE;
-        int []arr1=new int[n];
+        // int []arr1=new int[n];
+        // for(int i=0;i<n;i++){
+        //     arr1[i]=arr[i]-ans[i];
+        //     max=Math.max(arr1[i],max);
+        //     x=max;
+        // }
+        // return x;
+        int maxProfit=0;
         for(int i=0;i<n;i++){
-            arr1[i]=arr[i]-ans[i];
-            max=Math.max(arr1[i],max);
-            x=max;
+            for(int j=i+1;j<n;j++){
+                int profit=arr[j]-arr[i];
+                maxProfit=Math.max(maxProfit,profit);
+            }
         }
-        return x;
+        return maxProfit;
     }
 }
