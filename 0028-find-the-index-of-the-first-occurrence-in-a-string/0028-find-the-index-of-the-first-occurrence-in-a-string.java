4279f5ff10059c49dec63fda4d3b2862731da907
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-03-09 10:10:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,8 @@
 class Solution {
-    public int strStr(String s, String n) {
-        return s.indexOf(n);
+    public int strStr(String h, String n) {
+        for(int i=0;i<=h.length()-n.length();i++){
+                if(h.contains(n)) return i;
+            }
+        return -1;
     }
 }
