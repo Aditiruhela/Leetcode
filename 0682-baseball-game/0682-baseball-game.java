01d4f527c8ef14aac883011bfadc4ed70893ca4e
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-03-20 11:17:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,17 +1,16 @@
 class Solution {
     public int calPoints(String[] op) {
-        Stack<Integer>st=new Stack<>();
+        Stack <Integer>st=new Stack<>();
         for(String x:op){
             if(x.equals("+")){
                 int a=st.pop();
                 int b=st.peek();
                 st.push(a);
                 st.push(a+b);
-            }else if(x.equals("D")){
-                st.push(st.peek()*2);
-            }else if(x.equals("C")){
-                st.pop();
-            }else{
+            }
+            else if(x.equals("C")) st.pop();
+            else if(x.equals("D")) st.push(st.peek()*2);
+            else {
                 st.push(Integer.parseInt(x));
             }
         }
