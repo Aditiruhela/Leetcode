1c04fae96020c3b0fba1c18a794f8e6696fa3b36
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-03-26 08:52:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,14 +1,12 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {        
+    public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>>map=new HashMap<>();
-        for(String ch:strs){
-            char[]arr=ch.toCharArray();
+        for(String x:strs){
+            char []arr=x.toCharArray();
             Arrays.sort(arr);
             String key=new String(arr);
-            if(!map.containsKey(key)){
-                map.put(key,new ArrayList<>());
-            }
-            map.get(key).add(ch);
+            if(!map.containsKey(key)) map.put(key,new ArrayList<>());
+            map.get(key).add(x);
         }
         return new ArrayList<>(map.values());
     }
