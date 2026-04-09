29b899c20dfb9531e039426c48fa2f13be30f2d0
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-04-09 23:12:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,34 +1,20 @@
 class Solution {
-    public int[] nextGreaterElement(int[] arr1, int[] arr2) {
-        int n1 = arr1.length;
-        int n2 = arr2.length;
-
-        for (int i = 0; i < n1; i++) {
-            int target = arr1[i];
-            boolean found = false;
-            int nextGreater = -1;
-
-            // find target in arr2
-            for (int j = 0; j < n2; j++) {
-
-                if (arr2[j] == target) {
-                    found = true;
-
-                    // scan right side to find ANY next greater element
-                    for (int k = j + 1; k < n2; k++) {
-                        if (arr2[k] > target) {
-                            nextGreater = arr2[k];
-                            break;
-                        }
-                    }
-
-                    break;  // target matched, stop j loop
-                }
-            }
-
-            arr1[i] = nextGreater;
+    public int[] nextGreaterElement(int[]arr1, int[]arr2) {
+        Stack<Integer>st=new Stack<>();
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int x:arr2){
+            while(!st.isEmpty() && x>st.peek()) map.put(st.pop(),x);
+            st.push(x);
         }
-
-        return arr1;
+        while(st.size()!=0){
+            map.put(st.pop(),-1);
+        }
+        int []ans=new int[arr1.length];
+        int i=0;
+        for(int x:arr1){
+            ans[i]=map.get(x);
+            i++;
+        }
+        return ans;
     }
 }
