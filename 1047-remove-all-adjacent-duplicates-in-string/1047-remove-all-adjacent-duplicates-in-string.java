19a67b3a16fda64307e5714cc7e749123a9e1101
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-04-10 08:51:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,16 +1,4 @@
 class Solution {
-    public String reverse(String s){
-        char []arr=s.toCharArray();
-        int i=0,j=s.length()-1;
-        while(i<j){
-            char temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            i++;
-            j--;
-        }
-        return new String(arr);
-    }
     public String removeDuplicates(String s) {
         Stack<Character>st=new Stack<>();
         int n=s.length();
