8744ba0a8f994fc41619ec2cdc3039e1250bd93d
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-04-20 22:05:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -1,26 +1,30 @@
 class Solution {
     public int maxDistance(int[] arr) {
+        //brute
         int n=arr.length;
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(arr[i]!=arr[j]){
-                    max=Math.max(max,Math.abs(i-j));
-                }
-            }
-        }
-        return max;
-        // int i=0,j=n-1;
-        // int ans=-1;
-        // while(i<j){
-        //     if(arr[i]!=arr[j]){
-        //         ans=Math.abs(i-j);
-        //         return ans;
-        //     }else{
-        //         i++;
-        //         j--;
+        // int max=Integer.MIN_VALUE;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(arr[i]!=arr[j]){
+        //             max=Math.max(max,Math.abs(i-j));
+        //         }
         //     }
         // }
-        // return ans;
+        // return max;
+        int i=0,j=n-1;
+        int ans=-1;
+        while(j>0){
+            if(arr[i]!=arr[j]){
+                ans=Math.abs(i-j);
+               break;
+            }else j--;
+        }
+        while(i<n){
+            if(arr[i]!=arr[j]){
+                ans=Math.abs(i-j);
+                break;
+            }else i++;
+        }
+        return ans;
     }
 }
