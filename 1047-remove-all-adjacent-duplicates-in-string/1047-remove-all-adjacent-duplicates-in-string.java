251062e7e83437802b5a53b3e65c8c137360f666
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-04-24 11:53:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -3,8 +3,9 @@ class Solution {
         Stack<Character>st=new Stack<>();
         int n=s.length();
         for(int i=0;i<n;i++){
-            if(st.size()!=0 && s.charAt(i)==st.peek()) st.pop();
-            else st.push(s.charAt(i));
+            char ch=s.charAt(i);
+            if(st.size()!=0 && ch==st.peek()) st.pop();
+            else st.push(ch);
         }
         String ans="";
         for(char x:st) ans+=x;
