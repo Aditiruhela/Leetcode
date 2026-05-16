e529098a73aaf62d04c0e4c9f6f91f9e4a206a2d
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-05-16 19:13:32 +0530

    Time: 129 ms (7.39%), Space: 49.2 MB (5.54%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -4,11 +4,13 @@ class Solution {
         int n=s.length();
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
-            if(st.size()!=0 && ch==st.peek()) st.pop();
+            if(st.size()!=0 && ch==st.peek())st.pop();
             else st.push(ch);
         }
         String ans="";
-        for(char x:st) ans+=x;
+        for(char ch:st){
+            ans+=ch;
+        }
         return ans;
     }
 }
