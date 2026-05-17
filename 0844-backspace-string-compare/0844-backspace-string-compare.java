6898982456ee4d4959e86604ce9dfe28cf55c4c7
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-05-17 12:01:00 +0530

    Time: 5 ms (4.15%), Space: 44 MB (6.1%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -4,20 +4,21 @@ class Solution {
         Stack<Character>gt=new Stack<>();
         for(char x:s.toCharArray()){
             if(x=='#'){
-                if(!st.isEmpty()) st.pop();
+                if(!st.isEmpty())st.pop();
             }
             else st.push(x);
         }
         for(char x:t.toCharArray()){
             if(x=='#'){
-                if(!gt.isEmpty()) gt.pop();
+                if(!gt.isEmpty())gt.pop();
             }
             else gt.push(x);
         }
-        String ans1="";
-        String ans2="";
-        for(int x:st) ans1+=x;
-        for(int x:gt) ans2+=x;
-        return ans1.equals(ans2);
+        String str1="";
+        String str2="";
+        for(int x:st)str1+=x;
+        for(int x:gt)str2+=x;
+        if(str1.equals(str2)) return true;
+        else return false;
     }
 }
