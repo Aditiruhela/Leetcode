3dc2f2c3a3e71fdc94976569354f407e661d96b7
Aditiruhela.058 <ruhelaaditi9@gmail.com>
2026-05-17 18:33:35 +0530

    Time: 1 ms (95.62%), Space: 46.9 MB (21.79%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,19 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> q=new LinkedList<>();
-        List<List<Integer>>list=new ArrayList<>();
-        if(root!=null) q.add(root);
-        while(q.size()!=0){
+        List<List<Integer>>l=new ArrayList<>();
+        if(root==null) return l;
+        Queue<TreeNode>q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            ArrayList<Integer>level=new ArrayList<>();
             int size=q.size();
-           List<Integer> tempList=new ArrayList<>();
             for(int i=0;i<size;i++){
-                TreeNode temp=q.poll();
-                tempList.add(temp.val);
-            if(temp.left!=null) q.add(temp.left);
-            if(temp.right!=null) q.add(temp.right);
+                TreeNode node=q.remove();
+                level.add(node.val);
+                if(node.left!=null) q.add(node.left);
+                if(node.right!=null) q.add(node.right);
+            }
+            l.add(level);
         }
-        list.add(tempList);
-        }
-        return list;
+        return l;
     }
 }
